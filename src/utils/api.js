78f71706a4de33c6ab03e5cf353db02a91c56b79
Ndutech1 <136145477+Ndutech1@users.noreply.github.com>
2025-06-04 05:41:28 -1100

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,2 @@
-// utils/api.js
-const API_BASE = "https://movie-app-backend-3cyg.onrender.com"; // ✅ update to Render URL
+const API_BASE = 'https://movie-app-backend-3cyg.onrender.com/api';
 export default API_BASE;
